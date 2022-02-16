ec791183e3d47f9f3508c968b78b31b989db1b00
Keilet <6katika6@gmail.com>
2022-02-16 04:56:15 +0200

    Create table layout with holes

--- app/src/main/java/com/whac_a_mole/game/GameFragment.kt
+++ app/src/main/java/com/whac_a_mole/game/GameFragment.kt
@@ -2,6 +2,10 @@ package com.whac_a_mole.game
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
 import com.whac_a_mole.R
 import com.whac_a_mole._base.ViewBindingFragment
 import com.whac_a_mole.databinding.FragmentGameBinding
@@ -11,11 +15,19 @@ class GameFragment : ViewBindingFragment<FragmentGameBinding>(
     FragmentGameBinding::inflate
 ) {
 
+    lateinit var tableLayout: TableLayout
+    var images: ArrayList<ImageView> = arrayListOf()
+
     override fun onViewBindingCreated(
         viewBinding: FragmentGameBinding,
         savedInstanceState: Bundle?
     ) {
         super.onViewBindingCreated(viewBinding, savedInstanceState)
+
+        val FIELD_ROWS = 3
+        val FIELD_COLUMNS = 3
+
+        tableLayout = viewBinding.tableLayout
 
         object : CountDownTimer(31000, 1000) {
             override fun onTick(timeS: Long) {
@@ -34,5 +46,23 @@ class GameFragment : ViewBindingFragment<FragmentGameBinding>(
             }
 
         }.start()
+
+        for (i in 0 until FIELD_ROWS) {
+            val tableRow = TableRow(context)
+            tableRow.setLayoutParams(
+                ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
+            )
+            for (j in 0 until FIELD_COLUMNS) {
+                val imageView = ImageView(context)
+                images.add(imageView)
+                imageView.setImageResource(R.drawable.ic_hole)
+                tableRow.addView(imageView, j)
+
+            }
+            tableLayout.addView(tableRow, i)
+        }
     }
 }
